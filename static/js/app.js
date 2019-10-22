4e420d87fedfb62afdc51d60dcdcd8c10c45a6e6
davidm505 <damorales310@gmail.com>
2019-10-21 18:12:15 -0600

    Fixes Fixed bug where operation wouldn't complete if valid numbers were entered and operator button was clicked. Fixed bug where operator was allowed without any number being present. Fixed bug where multiple decimal points were allowed. Added decimalbutton listener.

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,12 @@
 const calculator = {
-    history: null,
-    num1: null,
-    num2: null, 
-    operator: null,
-    total: null,
-}
+  history: null,
+  num1: null,
+  num2: null,
+  operator: null,
+  total: null
+};
 
-const display = document.querySelector('#display');
+const display = document.querySelector("#display");
 
 const clearButton = document.querySelector("#clear");
 
@@ -14,125 +14,171 @@ const inputButtons = document.querySelectorAll(".input");
 
 const operatorButtons = document.querySelectorAll(".operator");
 
-const equalButton = document.querySelector('#equals');
+const equalButton = document.querySelector("#equals");
 
-clearButton.addEventListener('click', clearDisplay);
+const decimalButton = document.querySelector('.decimal');
+
+clearButton.addEventListener("click", clearDisplay);
 
 inputButtons.forEach(button => button.addEventListener("click", addToDisplay));
 
-operatorButtons.forEach(button => button.addEventListener('click', handleOperators));
+operatorButtons.forEach(button =>
+  button.addEventListener("click", handleOperators)
+);
 
-equalButton.addEventListener('click', handleEqual);
+equalButton.addEventListener("click", handleEqual);
 
+decimalButton.addEventListener('click', function(e) {
+  let value = display.innerHTML;
+
+  let dUsed = false;
+
+  if(!calculator.operator){
+    for(let i of value){
+      console.log(i);
+      if(i == '.')
+        dUsed = true;
+    }
+  } else {
+    let oIndex = value.indexOf(calculator.operator);
+
+    let afterOp = value.slice(oIndex + 1);
+
+    for(let i of afterOp){
+      console.log('after op' + i);
+
+      if(i == '.'){
+        dUsed = true;
+      }
+    }
+  }
+
+  if(!dUsed)
+    addToDisplay(e);
+})
 
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    let value = num1 * num2;
-    return value;
+  let value = num1 * num2;
+  return value;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+  if(num2 == 0)
+    return "Bad Boy!";
+  return num1 / num2;
 }
 
 function operate(operator, num1, num2) {
-    switch(operator) {
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return subtract(num1, num2);
-        case 'x':
-            return multiply(num1, num2);
-        case '/':
-            return divide(num1, num2);
-        default:
-            console.log("ERROR!");
-            break;
-    };
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "x":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return "ERROR!";
+  }
 }
 
 function addToCalc(e) {
-    if(!calculator.num1) {
-        calculator.num1 = Number(e);
-        console.log('Num1 is: ' + calculator.num1);
-    }
-    else {
-        calculator.num2 = Number(e);
-        console.log('Num2 is: ' + calculator.num2);
-    }
+  if(e == null)
+    e = '0';
+
+  console.log(e);
+
+  if (!calculator.num1) {
+    calculator.num1 = Number(e);
+    console.log("Num1 is: " + calculator.num1);
+  } else {
+    calculator.num2 = Number(e);
+    console.log("Num2 is: " + calculator.num2);
+  }
 }
 
 function addToDisplay(e) {
-    display.innerHTML += e.target.value;
+  display.innerHTML += e.target.value;
 }
 
 function clearDisplay() {
-    display.innerHTML = '';
-    calculator.num1 = null;
-    calculator.num2 = null;
-    calculator.operator = null;
-    calculator.total = null;
+  display.innerHTML = "";
+  calculator.num1 = null;
+  calculator.num2 = null;
+  calculator.operator = null;
+  calculator.total = null;
 }
 
 function handleOperators(e) {
-    let oIndex = 0;
-    
-    if(!calculator.operator) {
-        console.log(calculator);
+  let oIndex = 0;
 
-        addToDisplay(e);
-
-        oIndex = display.innerHTML.indexOf(e.target.value);
-        
-        calculator.operator = e.target.value;
-        
-        addToCalc(display.innerHTML.slice(0,oIndex));
+  if(display.innerHTML != '') {
+    if (!calculator.operator) {
+      addToDisplay(e);
+      
+      oIndex = display.innerHTML.indexOf(e.target.value);
+      
+      calculator.operator = e.target.value;
+      
+      addToCalc(display.innerHTML.slice(0, oIndex));
+  
+      console.log(calculator);
+    } else {
+      oIndex = display.innerHTML.indexOf(calculator.operator);
+  
+      addToCalc(display.innerHTML.slice(oIndex + 1));
+  
+      calculator.total = operate(
+        calculator.operator,
+        calculator.num1,
+        calculator.num2
+      );
+  
+      calculator.history = display.innerHTML.slice(0, -1);
+  
+      display.innerHTML = calculator.total;
+  
+      addToDisplay(e);
+  
+      calculator.num1 = calculator.total;
+      calculator.num2 = null;
+      calculator.operator = e.target.value;
+  
+      console.log(calculator);
     }
-    else {
-        oIndex = display.innerHTML.indexOf(calculator.operator);
-        
-        addToCalc(display.innerHTML.slice(oIndex + 1,-1));
-        
-        calculator.total = operate(calculator.operator, calculator.num1, calculator.num2);
-        
-        calculator.history = display.innerHTML.slice(0,-1);
 
-        display.innerHTML = calculator.total;
-
-        addToDisplay(e);
-        
-        calculator.num1 = calculator.total;
-        calculator.num2 = null;
-        calculator.operator = e.target.value;
-
-        console.log(calculator);
-    }
+  }
 }
 
 function handleEqual() {
-    let value = display.innerHTML;
-    
-    const operators = ['x', '+', '-', '/'];
-    
-    let oIndex = value.indexOf(calculator.operator);
-    
-    addToCalc(value.slice(oIndex + 1));
-    
-    calculator.total = operate(calculator.operator, calculator.num1, calculator.num2);
-    
-    calculator.history = value;
-    
-    console.log(calculator);
-    
-    display.innerHTML = calculator.total;
-    calculator.num1 = calculator.total;
-    calculator.num2 = null;
-    calculator.operator = null;
+  let value = display.innerHTML;
+
+  let oIndex = value.indexOf(calculator.operator);
+
+  addToCalc(value.slice(oIndex + 1));
+
+  calculator.total = operate(
+    calculator.operator,
+    calculator.num1,
+    calculator.num2
+  );
+
+  calculator.history = value;
+
+  
+  display.innerHTML = calculator.total;
+  calculator.num1 = calculator.total;
+  calculator.num2 = null;
+  calculator.operator = null;
+
+  console.log(calculator);
 }
